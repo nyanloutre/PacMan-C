36ae682c30bba62cd389003672f359f92cc3d04e
Paul TREHIOU <paul.trehiou@utbm.fr>
2014-05-23 21:53:29 +0200

    Pas possible de gérer le buffer
    Il n'existe pas de fonction permettant de régler le buffer et la taille
    de la police. Il faut demander de le faire à la main.

--- setwindow.c
+++ setwindow.c
@@ -2,10 +2,6 @@
 
 void SetWindow(int Largeur, int Hauteur)
 {
-    /*COORD coord;
-    coord.X = Largeur;
-    coord.Y = Hauteur;*/
-
     SMALL_RECT Rect;
     Rect.Top = 0;
     Rect.Left = 0;
@@ -13,6 +9,6 @@ void SetWindow(int Largeur, int Hauteur)
     Rect.Right = Largeur - 1;
 
     HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);      // Get Handle
-    //SetConsoleScreenBufferSize(Handle, coord);            // Set Buffer Size
+
     SetConsoleWindowInfo(Handle, TRUE, &Rect);            // Set Window Size
 }
