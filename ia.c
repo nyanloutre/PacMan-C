e43dbeaf6d021d4149ece7fb6a21b8c55b536633
Vskilet <victor.sene@utbm.fr>
2014-06-01 16:33:39 +0200

    Deuxième tentative non fonctionnelle

--- ia.c
+++ ia.c
@@ -4,28 +4,29 @@ int ia(coordonees pacman, coordonees fantome, char terrain[20][38])
 {
     int direction=0; // on a 1=Bas, 2=Gauche, 3=Haut, 4=Droite
 
+
     if (pacman.j < fantome.j)  //test pour savoir si le fantôme est à gauche du pacman
     {
-        if (pacman.i < fantome.i) //test pour savoir si le pacman est à gauche du fantôme
+        if (pacman.i < fantome.i) //test pour savoir si le pacman est en haut du fantôme
         {
-            if (terrain[fantome.i-1][fantome.j]==' ')
+            if (terrain[fantome.i-1][fantome.j]==' ') //si il y a pas de mur devant le fantôme alors ...
             {
                 direction = 3;
             }
-            else
+            else if (terrain[fantome.i][fantome.j-1]==' ')
             {
                 direction = 2;
             }
         }
         else
         {
-            if (terrain[fantome.i+1][fantome.j]==' ')
+            if (terrain[fantome.i-1][fantome.j]==' ') //si il y a pas de mur devant le fantôme alors ...
             {
-                direction = 1;
+                direction = 3;
             }
-            else
+            else if (terrain[fantome.i][fantome.j+1]==' ')
             {
-                direction = 2;
+                direction = 4;
             }
         }
     }
