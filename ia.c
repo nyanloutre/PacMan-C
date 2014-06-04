f955ada17db1aab86e997e949c76344efaffa444
Vskilet <victor.sene@utbm.fr>
2014-06-05 01:08:00 +0200

    Nouvelle tentative avec beaucoup de condition NON FONCTIONNEL

--- ia.c
+++ ia.c
@@ -1,42 +1,142 @@
 #include <math.h>
+#include <time.h>
 
 #include "structures.h"
-
+//Peut-être faudra-t-il supprimer dans les if principaux le old Direction et l'insérer dans la détermination de la direction (if le plus imbriqué)
 
 int ia(coordonees pacman, coordonees fantome, char terrain[20][38], int olDirection)
 {
     int direction=0; // on a 1=Bas, 2=Gauche, 3=Haut, 4=Droite
+    srand(time(NULL));
 
-    /*if (terrain[fantome.i-1][fantome.j]==' ' || terrain[fantome.i-1][fantome.j]=='.')                       //libre devant
+    if (terrain[fantome.i-1][fantome.j]==' ' || terrain[fantome.i-1][fantome.j]=='.')         //libre haut
     {
-        if (terrain[fantome.i][fantome.j-1]==' ' || terrain[fantome.i][fantome.j-1]=='.')                       //libre à devant,gauche
+        if (terrain[fantome.i][fantome.j-1]==' ' || terrain[fantome.i][fantome.j-1]=='.')          //libre à haut,gauche
         {
-            if (terrain[fantome.i+1][fantome.j]==' ' || terrain[fantome.i+1][fantome.j]=='.')                       //libre devant,gauche,bas
+            if (terrain[fantome.i+1][fantome.j]==' ' || terrain[fantome.i+1][fantome.j]=='.')                                //libre haut,gauche,bas
+            {
+
+                if (pacman.i-fantome.i<=0) //SI pacman en haut
+                {
+                    if (pacman.j-fantome.j<=0) //SI pacman à gauche
+                    {
+                        //le pacman ce trouve en haut à gauche du fantôme
+                        if (pacman.i-fantome.i==0)
+                        {
+                            direction = 2;
+                        }
+                        else if (pacman.j-fantome.j==0)
+                        {
+                            direction = 3;
+                        }
+                        else
+                        {
+                            direction = rand()%2+2;
+                        }
+                    }
+                    else
+                    {
+                        direction = 3;
+                    }
+                }
+                else //SI pacman en bas
+                {
+                    if (pacman.j-fantome.j<=0) //SI pacman à gauche
+                    {
+                        //le pacman ce trouve en bas à gauche du fantôme
+                        if (pacman.i-fantome.i==0)
+                        {
+                            direction = 2;
+                        }
+                        else if (pacman.j-fantome.j==0)
+                        {
+                            direction = 3;
+                        }
+                        else
+                        {
+                            direction = rand()%2+1;
+                        }
+                    }
+                    else
+                    {
+                        direction = 1;
+                    }
+                }
+
+            }
+            else if (terrain[fantome.i][fantome.j+1]==' ' || terrain[fantome.i][fantome.j+1]=='.')   //libre à haut,gauche,droite
             {
 
             }
-            else if (terrain[fantome.i][fantome.j+1]==' ' || terrain[fantome.i+1][fantome.j+1]=='.')                //libre à devant,gauche,droite
+            else
             {
-
+                direction = rand()%2+2;
             }
         }
-        else if (terrain[fantome.i][fantome.j+1]==' ' || terrain[fantome.i+1][fantome.j+1]=='.')                //libre à devant, droite
+        else if (terrain[fantome.i][fantome.j+1]==' ' || terrain[fantome.i][fantome.j+1]=='.')          //libre à haut, droite
         {
-            if (terrain[fantome.i+1][fantome.j]==' ' || terrain[fantome.i+1][fantome.j]=='.')                       //libre devant,droite,bas
+            if (terrain[fantome.i+1][fantome.j]==' ' || terrain[fantome.i+1][fantome.j]=='.')              //libre haut,droite,bas
             {
-
+                // A VOIR
             }
-            else if (terrain[fantome.i][fantome.j+1]==' ' || terrain[fantome.i+1][fantome.j+1]=='.')                //libre à devant,droite,
+            else
             {
-
+                direction = rand()%2+3;
             }
         }
-        else if ()
+        else if (terrain[fantome.i+1][fantome.j]==' ' || terrain[fantome.i+1][fantome.j]=='.') //libre en haut, bas
         {
 
         }
+        else
+        {
+            direction = 3;
+        }
 
-    }*/
+    }//FIN LIBRE HAUT
+    else if (terrain[fantome.i+1][fantome.j]==' ' || terrain[fantome.i+1][fantome.j]=='.')      //libre bas
+    {
+        if (terrain[fantome.i][fantome.j-1]==' ' || terrain[fantome.i][fantome.j-1]=='.') //libre bas, gauche
+        {
+            if (terrain[fantome.i][fantome.j+1]==' ' || terrain[fantome.i][fantome.j+1]=='.') //libre bas, gauche, droite
+            {
+                //A VOIR
+            }
+            else
+            {
+                direction = rand()%2+1;
+            }
+        }
+        else
+        {
+            direction = 1;
+        }
+
+    }//FIN LIBRE BAS
+    else if (terrain[fantome.i][fantome.j-1]==' ' || terrain[fantome.i][fantome.j-1]=='.')      //Libre gauche
+    {
+        if (terrain[fantome.i][fantome.j+1]==' ' || terrain[fantome.i][fantome.j+1]=='.')
+        {
+            if (pacman.j-fantome.j<=0) //SI pacman à gauche
+            {
+                direction = 2;
+            }
+            else
+            {
+                direction = 2;
+            }
+        }
+        else
+        {
+            direction = 2;
+        }
+
+    }
+    else if (terrain[fantome.i][fantome.j+1]==' ' || terrain[fantome.i][fantome.j+1]=='.')        //Libre droite
+    {
+        direction = 4;
+    }
+    /*---------------------------------------------------------
 
     if (pacman.i<=fantome.i)
     {
@@ -119,6 +219,6 @@ int ia(coordonees pacman, coordonees fantome, char terrain[20][38], int olDirect
                 direction = 3;
             }
         }
-    }
+    }*/
     return direction;
 }
